e880e76582ee00534e790d976151041a08e03be9
sangil <fhemflrh60@gmail.com>
2023-06-19 22:37:22 +0900

    로그 확인 및 인트로 코드 주석 해제 화면 미출력 오류 수정 예정

--- app/src/main/java/com/example/dailynews/activity/IntroActivity.kt
+++ app/src/main/java/com/example/dailynews/activity/IntroActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.os.PersistableBundle
-import android.util.Log
 import android.view.View
 import android.view.ViewTreeObserver
 import android.view.animation.AnticipateInterpolator
@@ -38,54 +37,54 @@ class IntroActivity : BaseActivity() {
                 setContentView(this)
             }
         }
-//        initSplashScreen()
+        initSplashScreen()
     }
 
-//    private fun initSplashScreen() {
-//        initData()
-//
-//        val content: View = findViewById(android.R.id.content)
-//        content.viewTreeObserver.addOnPreDrawListener(
-//            object : ViewTreeObserver.OnPreDrawListener {
-//                override fun onPreDraw(): Boolean {
-//                    // Check if the initial data is ready.
-//                    return if (isReady) {
-//                        // 3초 후 Splash Screen 제거
-//                        content.viewTreeObserver.removeOnPreDrawListener(this)
-//                        true
-//                    } else {
-//                        // The content is not ready
-//                        false
-//                    }
-//                }
-//            }
-//        )
-//
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-//            errorGuard()
-//
-//            splashScreen.setOnExitAnimationListener { splashScreenView ->
-//                val slideUp = ObjectAnimator.ofFloat(
-//                    splashScreenView,
-//                    View.TRANSLATION_Y,
-//                    0f,
-//                    -splashScreenView.height.toFloat()
-//                )
-//                slideUp.interpolator = AnticipateInterpolator()
-//                slideUp.duration = 1000L
-//                isStart = true
-//
-//                slideUp.doOnEnd {
-//                    splashScreenView.remove()
-//                    startMainActivity()
-//                }
-//
-//                slideUp.start()
-//            }
-//        } else {
-//            startMainActivity()
-//        }
-//    }
+    private fun initSplashScreen() {
+        initData()
+
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    // Check if the initial data is ready.
+                    return if (isReady) {
+                        // 3초 후 Splash Screen 제거
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else {
+                        // The content is not ready
+                        false
+                    }
+                }
+            }
+        )
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            errorGuard()
+
+            splashScreen.setOnExitAnimationListener { splashScreenView ->
+                val slideUp = ObjectAnimator.ofFloat(
+                    splashScreenView,
+                    View.TRANSLATION_Y,
+                    0f,
+                    -splashScreenView.height.toFloat()
+                )
+                slideUp.interpolator = AnticipateInterpolator()
+                slideUp.duration = 1000L
+                isStart = true
+
+                slideUp.doOnEnd {
+                    splashScreenView.remove()
+                    startMainActivity()
+                }
+
+                slideUp.start()
+            }
+        } else {
+            startMainActivity()
+        }
+    }
 
     private fun initData() {
         thread(start = true) {
