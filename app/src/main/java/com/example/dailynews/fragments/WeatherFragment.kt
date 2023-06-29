d08495eb87cb14ac87c9989f5e9127a383065789
sangil <fhemflrh60@gmail.com>
2023-06-29 20:35:01 +0900

    도시 받아오기 기능 구현 도시의 날씨로 구현 default 한 기능 구현 필요

--- app/src/main/java/com/example/dailynews/fragments/WeatherFragment.kt
+++ app/src/main/java/com/example/dailynews/fragments/WeatherFragment.kt
@@ -1,6 +1,10 @@
 package com.example.dailynews.fragments
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context.LOCATION_SERVICE
+import android.content.pm.PackageManager
+import android.location.Address
 import android.location.Geocoder
 import android.location.Location
 import android.location.LocationListener
@@ -10,7 +14,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.Observer
 import com.example.dailynews.R
 import com.example.dailynews.base.BaseFragment
@@ -20,6 +26,7 @@ import com.example.dailynews.tools.customDialog.CustomDialog
 import com.example.dailynews.tools.logger.Logger
 import org.json.JSONObject
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.io.IOException
 import java.util.Locale
 
 // 날씨 Fragment
@@ -29,10 +36,6 @@ class WeatherFragment : BaseFragment(R.layout.fragment_todo) {
 
     private val binding get() = _binding!!
     private lateinit var locationManager: LocationManager
-    private lateinit var locationListener: LocationListener
-
-    private var currentLat = 0.0
-    private var currentLong = 0.0
 
     private val viewModel by viewModel<WeatherViewModel>()
 
@@ -47,25 +50,45 @@ class WeatherFragment : BaseFragment(R.layout.fragment_todo) {
             container,
             false
         )
-        return binding.root
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
         locationManager = requireContext().getSystemService(LOCATION_SERVICE) as LocationManager
-        locationListener = LocationListener {
-            currentLat = it.latitude
-            currentLong= it.longitude
-        }
-
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setBinding()
         getCityName()
-        initWeatherInfoViewModel()
         observeData()
+        setObserver()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // 위치 권한이 없을 경우 권한 요청
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                REQUEST_LOCATION_PERMISSION
+            )
+        } else {
+            getCityName()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        locationManager.removeUpdates(locationListener)
     }
 
     override fun onDestroyView() {
@@ -74,14 +97,21 @@ class WeatherFragment : BaseFragment(R.layout.fragment_todo) {
     }
 
     private fun setBinding() {
-        with(binding) {}
+        with(binding) {
+        }
     }
 
-    private fun initWeatherInfoViewModel() {
+    private fun setObserver() {
+        viewModel.cityName.observe(viewLifecycleOwner) {
+            binding.weatherCityName.text = it
+        }
+    }
+
+    private fun initWeatherInfoViewModel(cityName: String) {
         val jsonObject = JSONObject()
         jsonObject.put("url", getString(R.string.weather_url))
         jsonObject.put("path", "weather")
-        jsonObject.put("q", "Seoul")
+        jsonObject.put("q", cityName)
         jsonObject.put("appid", getString(R.string.weather_key))
         viewModel.getWeatherInfoView(jsonObject)
     }
@@ -103,19 +133,18 @@ class WeatherFragment : BaseFragment(R.layout.fragment_todo) {
         )
     }
 
-    private fun getCityName(): String {
-//        locationManager = getSystemService
-        var cityName: String?
-//        val geoCoder = Geocoder(requireContext(), Locale.getDefault())
-//        val address = geoCoder.getFromLocation(lat, long, 1)
-//        cityName = address[0].adminArea
-//        if (cityName == null) {
-//            cityName = address[0].locality
-//            if (cityName == null) {
-//                cityName = address[0].subAdminArea
-//            }
-//        }
-        return "cityName"
+    @SuppressLint("MissingPermission")
+    private fun getCityName() {
+        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            locationManager.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER,
+                0,
+                0f,
+                locationListener
+            )
+        } else {
+            Toast.makeText(requireContext(), "위치 서비스를 사용할 수 없습니다.", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun setWeatherData(model: WeatherModel) {
@@ -129,5 +158,36 @@ class WeatherFragment : BaseFragment(R.layout.fragment_todo) {
 //            )
 //        ).placeholder(R.drawable.ic_launcher)
 //            .error(R.drawable.ic_launcher)
+    }
+
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            val latitude = location.latitude
+            val longitude = location.longitude
+
+            val geocoder = Geocoder(requireContext(), Locale.ENGLISH)
+            val geocoderDefault = Geocoder(requireContext(), Locale.getDefault())
+            try {
+                val addresses: List<Address> = geocoder.getFromLocation(latitude, longitude, 1)
+                if (addresses.isNotEmpty()) {
+                    val cityName = addresses[0].locality
+                    val cityNameDefault =
+                        geocoderDefault.getFromLocation(latitude, longitude, 1)[0].locality
+                    Logger.debug("DTE $cityName")
+                    viewModel.cityName.value = cityNameDefault
+                    initWeatherInfoViewModel(cityName)
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
+    }
+
+    companion object {
+        private const val REQUEST_LOCATION_PERMISSION = 1
     }
 }
