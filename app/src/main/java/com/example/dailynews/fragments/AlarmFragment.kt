30008085300719745651d546953f9cdf6467d17b
sangil <fhemflrh60@gmail.com>
2023-07-17 22:54:59 +0900

    Alarm 알람기능 구현

--- app/src/main/java/com/example/dailynews/fragments/AlarmFragment.kt
+++ app/src/main/java/com/example/dailynews/fragments/AlarmFragment.kt
@@ -1,5 +1,11 @@
 package com.example.dailynews.fragments
 
+import android.annotation.SuppressLint
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +13,14 @@ import android.view.ViewGroup
 import com.example.dailynews.R
 import com.example.dailynews.base.BaseFragment
 import com.example.dailynews.databinding.FragmentAlarmBinding
+import com.example.dailynews.tools.receiver.AlarmReceiver
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
 
 class AlarmFragment : BaseFragment(R.layout.fragment_alarm) {
 
@@ -15,6 +28,9 @@ class AlarmFragment : BaseFragment(R.layout.fragment_alarm) {
 
     private val viewModel by viewModel<AlarmViewModel>()
     private val binding get() = _binding!!
+
+    private lateinit var pendingIntent: PendingIntent
+    private val ioScope by lazy { CoroutineScope(Dispatchers.IO) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,5 +57,68 @@ class AlarmFragment : BaseFragment(R.layout.fragment_alarm) {
 
     private fun setBinding() {
         with(binding) {}
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    fun callAlarm(time: String, alarmCode: Int, content: String) {
+
+        val alarmManager = context?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val receiverIntent = Intent(context, AlarmReceiver::class.java)
+
+        receiverIntent.apply {
+            putExtra("alarmRqCode", alarmCode)
+            putExtra("content", content)
+        }
+
+        val pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            PendingIntent.getBroadcast(
+                context,
+                alarmCode,
+                receiverIntent,
+                PendingIntent.FLAG_IMMUTABLE
+            )
+        } else {
+            PendingIntent.getBroadcast(
+                context,
+                alarmCode,
+                receiverIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+        }
+
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd H:mm:ss")
+        var dateTime = Date()
+        try {
+            dateTime = dateFormat.parse(time) as Date
+        } catch (e: ParseException) {
+            e.printStackTrace()
+        }
+
+        val calendar = Calendar.getInstance()
+        calendar.time = dateTime
+
+        alarmManager.setExactAndAllowWhileIdle(
+            AlarmManager.RTC_WAKEUP,
+            calendar.timeInMillis,
+            pendingIntent
+        )
+    }
+
+    fun cancelAlarm(alarmCode: Int) {
+        val alarmManager = context?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, AlarmReceiver::class.java)
+
+        pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            PendingIntent.getBroadcast(context, alarmCode, intent, PendingIntent.FLAG_IMMUTABLE)
+        } else {
+            PendingIntent.getBroadcast(
+                context,
+                alarmCode,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+        }
+
+        alarmManager.cancel(pendingIntent)
     }
 }
