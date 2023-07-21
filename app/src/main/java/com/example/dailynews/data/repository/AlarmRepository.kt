72862dcb042549122ffedaf06ad0f08575e220c1
sangil <fhemflrh60@gmail.com>
2023-07-21 22:06:23 +0900

    알람메시지 구동 확인

--- app/src/main/java/com/example/dailynews/data/repository/AlarmRepository.kt
+++ app/src/main/java/com/example/dailynews/data/repository/AlarmRepository.kt
@@ -12,21 +12,29 @@ class AlarmRepository(
 ) : BaseRepository() {
 
     fun storeAlarmList(alarmItemsModel: AlarmItemsModel): List<AlarmItemsModel> {
+        // 원래 있던 알람 리스트
         val oldList = if (sharedPreferenceManager.alarmList == "") JSONArray()
         else JSONArray(sharedPreferenceManager.alarmList)
+        // 새로운 알람 리스트
         val newList = JsonArray()
+        // 반환할 알람 데이터 모델
         val newModel = mutableListOf<AlarmItemsModel>()
+        // 새로운 알람 리스트에 기존 알람 추가
         for (i in 0 until oldList.length()) {
             newList.add(oldList.optString(i))
         }
+        // 새로운 알람 리스트에 새로운 알람 추가
         newList.add(alarmItemsModel.time + alarmItemsModel.alarmCode.toString() + alarmItemsModel.content)
+        // 저장소에 새로운 알람 리스트 덮어쓰기
         sharedPreferenceManager.alarmList = newList.toString()
-        for (i in 0..oldList.length()) {
+        // 새로운 알람 모델에 기존 알람을 모델로 변환해 추가
+        for (i in 0 until oldList.length()) {
             newModel.add(
                 AlarmItemsModel(
-                    time = oldList.optString(i).substring(0, 3),
+                    time = oldList.optString(i).substring(0, 4),
                     alarmCode = oldList.optString(i).substring(4, 13).toInt(),
-                    content = oldList.optString(i).substring(14)?:"",
+                    content = if (oldList.optString(i).length > 13) oldList.optString(i)
+                        .substring(13) else "",
                 )
             )
         }
